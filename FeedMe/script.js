441e4850cf62a92d0e9a9d78d222aa38caa24110
Marlabon <43890308+Marlabon@users.noreply.github.com>
2019-12-15 20:50:22 +0100

    Add files via upload

--- FeedMe/script.js
+++ FeedMe/script.js
@@ -1,24 +1,26 @@
 // Wochentag und Datum anzeigen ****************************
 var curDate = new Date(); 
 // Baut die Anzeige mit Wochentag
-var dayString = curDate.getDay()-1;
+var dayString = curDate.getDay();
 var month = curDate.getMonth()+1;
+
+
 
 //Zahl des Wochentags in String mit Wochentag ändern
 if(dayString==0){
-    dayString ="Montag";
+    dayString ="Sunday";
 }else if(dayString==1){
-    dayString ="Dienstag";
+    dayString ="Monday";
 }else if(dayString==2){
-    dayString ="Mittwoch";
+    dayString ="Tuesday";
 }else if(dayString==3){
-    dayString ="Donnerstag";
+    dayString ="Wednesday";
 }else if(dayString==4){
-    dayString ="Freitag";
+    dayString ="Thursday";
 }else if(dayString==5){
-    dayString ="Samstag";
+    dayString ="Friday";
 }else if(dayString==6){
-    dayString ="Sonntag";
+    dayString ="Saturday";
 }
 
 document.getElementById("displayCurrentDate").innerHTML = dayString+" - "+curDate.getDate()+"."+month+"."+curDate.getFullYear();
